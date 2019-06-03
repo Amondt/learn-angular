7824b5e04944c0723019e2fd249c9b16e060254e
amondt <mondt.alexandre@gmail.com>
2019-06-03 16:52:11 +0200

    get started routing setup

--- angularGetStarted/src/app/app.module.ts
+++ angularGetStarted/src/app/app.module.ts
@@ -1,22 +1,26 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser'
+import { NgModule } from '@angular/core'
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { ProductListComponent } from './product-list/product-list.component';
-import { TopBarComponent } from './top-bar/top-bar.component';
+import { AppRoutingModule } from './app-routing.module'
+import { AppComponent } from './app.component'
+import { TopBarComponent } from './top-bar/top-bar.component'
+import { ProductListComponent } from './product-list/product-list.component'
+import { ProductDetailsComponent } from './product-details/product-details.component'
+import { ProductAlertComponent } from './product-alert/product-alert.component'
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    ProductListComponent,
-    TopBarComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent,
+        TopBarComponent,
+        ProductListComponent, 
+        ProductDetailsComponent, 
+        ProductAlertComponent
+    ],
+    imports: [
+        BrowserModule,
+        AppRoutingModule,
+    ],
+    providers: [],
+    bootstrap: [AppComponent]
 })
 export class AppModule { }
