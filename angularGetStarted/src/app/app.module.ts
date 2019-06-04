87e9b5c7fca7919b974f20edda13446b64a41458
amondt <mondt.alexandre@gmail.com>
2019-06-04 10:23:52 +0200

    angular get started cart / details / shipping

--- angularGetStarted/src/app/app.module.ts
+++ angularGetStarted/src/app/app.module.ts
@@ -1,12 +1,15 @@
 import { BrowserModule } from '@angular/platform-browser'
 import { NgModule } from '@angular/core'
-
 import { AppRoutingModule } from './app-routing.module'
+import { HttpClientModule } from '@angular/common/http'
+
 import { AppComponent } from './app.component'
 import { TopBarComponent } from './top-bar/top-bar.component'
 import { ProductListComponent } from './product-list/product-list.component'
 import { ProductDetailsComponent } from './product-details/product-details.component'
-import { ProductAlertComponent } from './product-alert/product-alert.component'
+import { ProductAlertComponent } from './product-alert/product-alert.component';
+import { CartComponent } from './cart/cart.component';
+import { ShippingComponent } from './shipping/shipping.component'
 
 @NgModule({
     declarations: [
@@ -14,11 +17,14 @@ import { ProductAlertComponent } from './product-alert/product-alert.component'
         TopBarComponent,
         ProductListComponent, 
         ProductDetailsComponent, 
-        ProductAlertComponent
+        ProductAlertComponent, 
+        CartComponent, 
+        ShippingComponent
     ],
     imports: [
         BrowserModule,
         AppRoutingModule,
+        HttpClientModule
     ],
     providers: [],
     bootstrap: [AppComponent]
--- angularGetStarted/src/app/product-alert/product-alert.component.ts
+++ angularGetStarted/src/app/product-alert/product-alert.component.ts
@@ -1,19 +1,19 @@
 import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 
 @Component({
-  selector: 'app-product-alert',
-  templateUrl: './product-alert.component.html',
-  styleUrls: ['./product-alert.component.css']
+    selector: 'app-product-alert',
+    templateUrl: './product-alert.component.html',
+    styleUrls: ['./product-alert.component.css']
 })
 
 export class ProductAlertComponent implements OnInit {
 
-  @Input() product
-  @Output() notify = new EventEmitter
+    @Input() product
+    @Output() notify = new EventEmitter
 
-  constructor() { }
+    constructor() { }
 
-  ngOnInit() {
-  }
+    ngOnInit() {
+    }
 
 }
